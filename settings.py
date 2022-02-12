30696ebaf3973393952e7d9de73fe259105b7a91
Hugo Demenez <hdemenez@hotmail.fr>
2022-02-12 17:34:39 +0100

    increasing timeframe analysis

--- settings.py
+++ settings.py
@@ -10,17 +10,17 @@ class Settings:
             self.ftx()
         elif self.broker_name == 'binance':
             self.binance()
-            
+
         self.backtesting = False
         self.base_asset = 'USD'
         broker = getattr(importlib.import_module('Python_Brokers_API'), '%s'%self.broker_name)
         self.broker = broker()
-        self.path = self.broker_name +".key"
+        self.path = f'{self.broker_name}.key'
         self.expected_yield = 1 + 0.1/100
-        self.real_expected_yield = self.expected_yield + 2*self.fee 
+        self.real_expected_yield = self.expected_yield + 2*self.fee
         self.risk = 1 - 5/100
         self.program_risk = 10/100
-        self.timeframe = 15
+        self.timeframe = 30
         self.prediction_time = 15
         
         
