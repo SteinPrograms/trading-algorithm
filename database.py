272cf604460b3df37dd71bc7900be2504e4f9452
Hugo Demenez <71768413+hugodemenez@users.noreply.github.com>
2022-05-10 16:22:54 +0200

    update database.py

--- database.py
+++ database.py
@@ -1,23 +1,21 @@
-import pymongo
+import requests
 
 
 class Database:
     def __init__(self):
-        password = "Manonhugo147"
-        username = "hugodemenez"
-        uri = f"mongodb+srv://{username}:{password}@test.yqzxd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
-        self.client = pymongo.MongoClient(uri)
+        self.endpoint = "http://hugodemenez.fr/api/"
         
     def get_target_value(self,symbol:str) -> float:
-        db = self.client.Trading
+        
         try:
-            return float(db.Target.find_one({"symbol":symbol})['target'])
+            return float(requests.get(f"{self.endpoint}{symbol}").json()['target'])
         except Exception:
             return 0.0
         
     def get_expected_yield(self,symbol:str) -> float:
-        db = self.client.Trading
         try:
-            return float(db.Target.find_one({"symbol":symbol})['yield'])
+            return float(requests.get(f"{self.endpoint}{symbol}").json()['yield'])
         except Exception:
             return 0.0
+
+print(Database().get_target_value("BTC/USD"))
