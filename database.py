ef2cf26bd62d714ee07fb1b5ef1f3af14544a8a5
Hugo Demenez <71768413+hugodemenez@users.noreply.github.com>
2022-05-28 11:35:43 +0200

    update database.py

--- database.py
+++ database.py
@@ -1,5 +1,8 @@
-import requests,json
 
+
+
+"""
+import requests,json
 
 class Database:
     def __init__(self):
@@ -41,6 +44,62 @@ class Database:
             
         except Exception as error:
             print(error)
-        
+"""
 
-print(Database().publish_position_data({'test':"test"}))
+
+import pymongo
+
+class Database:
+    def __init__(self):
+        password = "Manonhugo147"
+        username = "hugodemenez"
+        self.uri = f"mongodb+srv://{username}:{password}@test.yqzxd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+
+        
+    def get_target_value(self,symbol:str) -> float:
+        with pymongo.MongoClient(self.uri) as client:
+            try:
+                db = client.Trading
+                return float(db.Target.find_one({"symbol":symbol})['target'])
+            except Exception:
+                return 0.0
+        
+    def get_expected_yield(self,symbol:str) -> float:
+        with pymongo.MongoClient(self.uri) as client:
+            try:
+                db = client.Trading
+                return float(db.Target.find_one({"symbol":symbol})['yield'])
+            except Exception:
+                return 0.0
+        
+    def publish_server_data(self,data:dict) -> int:
+        with pymongo.MongoClient(self.uri) as client:
+            try:
+                db = client.Trading
+                db.server.update_one(filter={},update={'$set':data},upsert=True)
+            except Exception as error:
+                print(error)
+
+            
+
+    def get_server_data(self) -> dict:
+        with pymongo.MongoClient(self.uri) as client:
+            try:
+                db = client.Trading
+                return db.server.find_one(filter={})
+            
+            except Exception as error:
+                return error
+
+
+    def publish_position_data(self,data:dict) -> int:
+        with pymongo.MongoClient(self.uri) as client:
+            try:
+                db = client.Trading
+                db.Positions.insert_one(data)
+            except Exception as error:
+                print(error)
+        print(db.Positions.find_one({}))
+
+
+print(Database().publish_position_data({'hugo':'man'}))
