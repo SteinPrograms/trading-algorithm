2c24cd68fbbee748d9393dee3145feb4581de2e3
Hugo Demenez <hdemenez@hotmail.fr>
2022-07-24 14:16:45 +0100

    getting the close price from market

--- app/position.py
+++ app/position.py
@@ -140,9 +140,12 @@ class Position:
         # Stop loss
         # Close position :
         if self.current_effective_yield < 1-settings.RISK:
-            self.close_price = self.open_price * settings.RISK
+            self.close_price = self.open_price * (1+settings.RISK)
             if not self.backtesting:
-                RealCommands().market_close(self.symbol, backtesting=self.backtesting)
+                order = RealCommands().market_close(self.symbol, backtesting=self.backtesting)
+                avg_fill_price = order.get('order',{}).get('avgFillPrice',None)
+                if not avg_fill_price is None:
+                    self.close_price = avg_fill_price
             self.close_mode = "stop-loss"
             self.close_position()
             return
@@ -155,6 +158,9 @@ class Position:
                 self.close_price = self.current_price
                 if not self.backtesting:
                     RealCommands().market_close(symbol=self.symbol, backtesting=self.backtesting)
+                    avg_fill_price = order.get('order',{}).get('avgFillPrice',None)
+                    if not avg_fill_price is None:
+                        self.close_price = avg_fill_price
                 self.close_mode = "take-profit"
                 self.close_position()
                 return
