6603a1713024fe02cb0241e12bbc3a90c4e49e4e
hugodemenez <hdemenez@hotmail.fr>
2023-10-23 16:59:38 +0200

    saving progresses before loosing files

--- reporter/test.py
+++ reporter/test.py
@@ -1,12 +1,27 @@
-import sched, time
+from langchain.llms import Ollama
+from langchain.callbacks.manager import CallbackManager
+from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler                                  
+from langchain.prompts.chat import ChatPromptTemplate
 
-schedule = sched.scheduler(time.monotonic, time.sleep)
 
-def fetch():
-    print("Seems to be working")
-    schedule.enter(1,1,fetch)
-    schedule.run()
+llm = Ollama(model="mistral", 
+             )
 
-fetch()
-while True:
-    pass
+# First define model role and goal
+template = (
+    "Provided a list of news about the {symbol} crypto market."
+    "Write a blog post about the news."
+    "Make a general readable summary of around 500 characters about the news, make transition between news. It should be easy to read"
+)
+print(type(template))
+human_template = "{text}"
+
+chat_prompt = ChatPromptTemplate.from_messages([
+    ("system",template),
+    ("human",human_template),
+])
+
+chat = chat_prompt.format_messages(symbol="BTC",text="All time high for bitcoin")
+print(chat)
+# Second provide data that needs to be processed
+print(llm(chat))
