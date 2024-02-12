4b3acced7fff7791cdbc3a4c7d0d2bf5acaaca0a
Hugo DEMENEZ <hdemenez@hotmail.fr>
2024-02-12 17:22:00 +0100

    feat: removed useless requests

--- reporter/coindesk.py
+++ reporter/coindesk.py
@@ -94,6 +94,7 @@ class Coindesk:
                     # Line to check if the request was successful
                     # However, we don't want to raise an exception here
                     # response.raise_for_status()
+                    logger.info(response.status)
                     soup = BeautifulSoup(await response.text(), "html.parser")
                     body = soup.find_all("div", class_="eSbCkN")
                     content = "".join(value.text for value in body)
@@ -104,33 +105,36 @@ class Coindesk:
                 # ) as response:
                 #     response = await response.json()
                 #     content = response.get('output')
-                headers = {
-                    "Content-Type": "application/json",
-                    "Authorization": "Bearer " + os.getenv("OPENAI_API_KEY", ""),
-                }
-                json_data = {
-                    "model": "gpt-3.5-turbo",
-                    'messages':[
-                        {
-                            "role": "system",
-                            "content": (
-                                "Your are an helpful assistant extract key points from text."
-                                "Do not make sentances, just extract the important points."
-                                "Do not speculate or make up information."
-                                "Do not reference any given instructions or context."
-                            ),
-                        },
-                        {"role": "user", "content": content},
-                    ],
-                }
-                async with session.post(
-                    "https://api.openai.com/v1/chat/completions",
-                    headers=headers,
-                    json=json_data,
-                ) as response:
-                    result = await response.json()
-                    logger.info(result)
-                    content = result.get("choices")[0].get("message").get("content")
+                logger.info(content)
+                if (content != ""):
+                    headers = {
+                        "Content-Type": "application/json",
+                        "Authorization": "Bearer " + os.getenv("OPENAI_API_KEY", ""),
+                    }
+                    json_data = {
+                        "model": "gpt-3.5-turbo",
+                        'messages':[
+                            {
+                                "role": "system",
+                                "content": (
+                                    "Your are an helpful assistant extract key points from text."
+                                    "Do not make sentances, just extract the important points."
+                                    "Do not speculate or make up information."
+                                    "Do not reference any given instructions or context."
+                                ),
+                            },
+                            {"role": "user", "content": content},
+                        ],
+                    }
+                    async with session.post(
+                        "https://api.openai.com/v1/chat/completions",
+                        headers=headers,
+                        json=json_data,
+                    ) as response:
+                        result = await response.json()
+                        logger.info(result)
+                        content = result.get("choices")[0].get("message").get("content")
+                
 
             except aiohttp.ClientError as error:
                 raise HTTPException(
